e5b1601bd6700c777be824085f4f77efc8693bdd
Lysy18 <taborbartlomiej@gmail.com>
2023-11-19 13:00:10 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const { Server } = require("socket.io");
+// const express = require('express');
+// const app = express();
+const httpServer = http.createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User: ${socket.id} connected`);
+
+  // Obsługa stworzenia i dołączenia do pokoju
+  socket.on("createRoom", (roomName) => {
+    socket.join(roomName);
+    console.log(
+      `Klient ${socket.id} stworzył/połączył się z pokojem: ${roomName}`
+    );
+  });
+
+  socket.on("message", (data) => {
+    console.log(socket.id, data);
+    io.emit(`message`, `${socket.id.substring(0, 5)}: ${data}`);
+    io.emit(`id`, `${socket.id.substring(0, 5)}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Klient odłączony:', socket.id);
+  });
+});
+
+httpServer.listen(3500, () => console.log("listening on port 3500"));
