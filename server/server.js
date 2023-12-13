c45fc2af69ce66a54ddd574b1da6543bc6e39a33
Lysy18 <taborbartlomiej@gmail.com>
2023-12-13 22:53:39 +0100

    fgn

--- server/server.js
+++ server/server.js
@@ -144,7 +144,9 @@ io.on("connection", (socket) => {
     }
     return null;
   }
-
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
   // Dodaj funkcję, która aktualizuje ilość osób w danym pokoju
   function updateOccupancy(roomName, newOccupancy) {
     roomsAttributes[roomName].occupancy = newOccupancy;
