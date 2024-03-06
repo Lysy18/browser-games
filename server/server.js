998f8e9fae46bd0c1f4157fa06a61651c582e8c6
Lysy18 <taborbartlomiej@gmail.com>
2024-03-06 22:22:04 +0100

    rr

--- server/server.js
+++ server/server.js
@@ -5,9 +5,8 @@ const httpServer = http.createServer();
 const { createClient } = require("@supabase/supabase-js");
 
 // Zastąp poniższe dane swoimi danymi z Supabase
-const supabaseUrl = "https://tpiobmfcjontnvmvkrzo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwaW9ibWZjam9udG52bXZrcnpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MTQyODEsImV4cCI6MjAxNjQ5MDI4MX0.5JcyQimgLc0EmirbcpiqDmO3hMAimMrWR1KoEVDWQWM";
+const supabaseUrl = "";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 let rooms = [];
